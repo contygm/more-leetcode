93c10a88abf8df6187a7e55f59b1db7e21b967ac
contygm <contygm@gmail.com>
2021-11-02 10:50:24 -0700

    bs: #4 passes base cases not all

--- binarySearch/most-freq.ts
+++ binarySearch/most-freq.ts
@@ -52,3 +52,66 @@ class Solution {
         return right;
     }
 }
+
+// 4. Median of Two Sorted Arrays
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {    
+    let left = 0,
+        right = nums1.length;
+        
+    while(left <= right) {
+        const xMid = ~~((left+right)/2);
+        const yMid = ~~((nums1.length + nums2.length)/2) - xMid;
+        
+        const maxLeftX = nums1[xMid - 1] ?? Number.NEGATIVE_INFINITY;
+        const maxLeftY = nums2[yMid - 1] ?? Number.NEGATIVE_INFINITY;
+        
+        const minRightX = nums1[xMid] ?? Number.POSITIVE_INFINITY;
+        const minRightY = nums2[yMid] ?? Number.POSITIVE_INFINITY;
+        
+        // console.log("maxLeftX: ", maxLeftX, " minRightX: ", minRightX); 
+        // console.log("maxLeftY: ", maxLeftY, " minRightY: ", minRightY); 
+        
+        if( maxLeftX <= minRightY && maxLeftY <= minRightX) { // found
+            const rightMin = Math.min(minRightX, minRightY);
+            // console.log("leftMax ", leftMax);
+            // console.log("rightMin ", rightMin);
+            
+            // odd
+            if((nums1.length + nums2.length) % 2 === 1) { 
+                return rightMin;
+            }
+            // even
+            // console.log("even endd ", (( leftMax + Math.min(minRightX, minRightY) )/2));
+            return (( Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY) )/2);
+        } else if ( maxLeftY >  minRightX) {
+            // x ->
+            left = xMid + 1; 
+        } else {
+            // <- x
+            right = xMid - 1;
+        }
+    }
+    
+    return -1;
+};
+// base
+// [1,3]
+// [2]
+
+// [1,2]
+// [3,4]
+
+// [0,0]
+// [0,0]
+
+// []
+// [1]
+
+// [2]
+// [] ***
+// failing ones 
+// [1,2,3,5,6]
+// [4]
+
+// [3,4,5,6]
+// [1,2]
