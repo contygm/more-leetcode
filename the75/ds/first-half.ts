074e38989ee9d61b8e7c3f32c58cc8c12dd6e0f9
contygm <contygm@gmail.com>
2022-01-05 13:37:27 -0800

    working thru ds problems

--- the75/ds/first-half.ts
+++ the75/ds/first-half.ts
@@ -1,5 +1,5 @@
 /**
- * Definition for singly-linked list.
+ * Definition for **SINGLY** LINKED LIST.
  * class ListNode {
  *     val: number
  *     next: ListNode | null
@@ -10,4 +10,73 @@
  * }
  */
 
+
+ class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
 // 206. Reverse Linked List
+function reverseList(head: ListNode | null): ListNode | null {
+    
+    let newHead: ListNode = null;
+    
+    while(head) {
+        const temp: ListNode = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = temp;
+    }
+    
+    return newHead;
+};
+
+// 141. Linked List Cycle
+// Return true if there is a cycle in the linked list
+function hasCycle(head: ListNode | null): boolean {
+    
+    if(!head) return false;
+    
+    let slow: ListNode = head;
+    let fast: ListNode = head.next;
+    
+    while(fast !== slow) {
+        // reached end, no cycle
+        if(!fast || !fast.next) {
+            return false;
+        }
+        
+        // update pointers
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return true;
+};
+
+// 11. Container With Most Water
+// Return the maximum amount of water a container can store.
+function maxArea(height: number[]): number {
+    let max: number = 0;
+    let left: number = 0;
+    let right: number = height.length - 1;
+    
+    while(left < right) {
+        const length = right - left;
+        const currHeight = Math.min(height[right], height[left]);
+        
+        max = Math.max(max, length*currHeight);
+        
+        if(height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return max;
+};
