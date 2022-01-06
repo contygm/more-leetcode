a970001986d6b04f1eb3a294cfb3c9148b959e0c
contygm <contygm@gmail.com>
2022-01-06 12:42:43 -0800

    first half of ds

--- the75/ds/first-half.ts
+++ the75/ds/first-half.ts
@@ -80,3 +80,94 @@ function maxArea(height: number[]): number {
     
     return max;
 };
+
+// 153. Find Minimum in Rotated Sorted Array
+// return min of rotated sorted array
+function findMin(nums: number[]): number {
+    
+    let left: number = 0;
+    let right: number = nums.length - 1;
+    
+    // nums in asc order OR arr length === 1
+    if(nums[right] >= nums[left]) {
+        return nums[left];
+    }
+    
+    while(left <= right) {
+        const mid = left + ~~((right-left) / 2);
+        
+        // found inflection point -> right of mid
+        if(nums[mid] > nums[mid+1]) {
+            return nums[mid+1];
+        }
+        
+        // found inflection point -> left of mid
+        if(nums[mid-1] > nums[mid]) {
+            return nums[mid];
+        }
+        
+        if(nums[left] <= nums[mid]) { // go right
+           left = mid + 1;
+        } else { // left
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+};
+
+// 424. Longest Repeating Character Replacement
+// sliding window
+// Return the length of the longest substring containing 
+// the same letter you can get after K replacements
+function characterReplacement(s: string, k: number): number {
+    let left: number = 0;
+    let hash: Map<string, number> = new Map();
+    let maxCharCount: number = 0;
+    let maxLength: number = 0;
+    
+    for(let right = 0; right < s.length; right++) {
+        const char = s[right];
+        
+        if(!hash.has(char)) {
+            hash.set(char, 0);
+        }
+        
+        hash.set(char, (hash.get(char) + 1));
+        
+        maxCharCount = Math.max(maxCharCount, hash.get(char));
+        
+        if( (right-left+1 - maxCharCount) > k ) {
+            hash.set(s[left], (hash.get(s[left]) - 1));
+            left++;
+        }
+        
+        maxLength = Math.max(maxLength, right-left+1);
+    }
+    
+    return maxLength;
+};
+
+// 3. Longest Substring Without Repeating Characters
+// return the length of the longest substring without repeating characters
+function lengthOfLongestSubstring(s: string): number {
+    let maxLength: number = 0;
+    let letterSet: Set<string> = new Set();
+    let left: number = 0;
+    
+    for(let right = 0; right < s.length; right++) {
+        const char: string = s[right];
+
+        // adjust window if repeat char
+        while(letterSet.has(char)) {
+            letterSet.delete(s[left]);
+            left++;
+        }
+        
+        // add current char, update length
+        letterSet.add(char);
+        maxLength = Math.max(maxLength, right-left+1);
+    }
+    
+    return maxLength;
+};
---
+++ the75/ds/second-half.ts
@@ -0,0 +1,3 @@
+// 76. Minimum Window Substring
+
+// 200. Number of Islands
